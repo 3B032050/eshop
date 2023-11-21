8222f5105f528a76ee5e1568c1d854ff292f0b78
黃文輝 <antory040512@gmail.com>
2023-11-21 22:19:58 +0800

    幫ProductControllers七個方法路由添加註解

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+/*
+products.index url:/ http動詞:get 路由ProductController::class,'index'
+products.show url:prodcucts http動詞:get 路由ProductController::class,'show'
+products.create url:prodcucts/create http動詞:get 路由ProductController::class,'create'
+products.store url:prodcucts/store http動詞:post 路由ProductController::class,'store'
+products.edit url:prodcucts/{products}/edit http動詞:get 路由ProductController::class,'edit'
+products.update url:prodcucts/{products} http動詞:patch 路由ProductController::class,'update'
+products.destroy url:prodcucts/{products} http動詞:delete 路由ProductController::class,'ddestroy'
+*/
+Route::resource('Products', ProductController::class);
+
