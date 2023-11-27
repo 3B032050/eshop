49b752869b69c923e84b36dffc5d20cf5af20953
黃文輝 <antory040512@gmail.com>
2023-11-27 18:11:33 +0800

    安裝使用者身分驗證相關套件與功能

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,10 +15,6 @@ use App\Http\Controllers\ProductController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
 /*
 products.index url:/ http動詞:get 路由ProductController::class,'index'
 products.show url:prodcucts http動詞:get 路由ProductController::class,'show'
@@ -27,25 +26,27 @@ products.destroy url:prodcucts/{products} http動詞:delete 路由ProductControl
 列出7個Products路由應該有的作用
 products.index
 index方法的作用是顯示資源清單。
-
 products.show
 show方法的作用是顯示指定資源。
-
 products.create
 create方法的作用是顯示用於建立新資源的表單。
-
 products.store
 store方法的作用是將新建立的資源儲存在儲存中。
-
 products.edit
 edit方法的作用是顯示編輯指定資源的表單
-
 products.update
 update方法的作用是更新儲存中的指定資源。
-
 products.destroy
 destroy方法的作用是從儲存中刪除指定的資源。
 */
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::get('Products',[ProductController::class,'index'])->name("products.index");
 Route::get('Products/{Product}',[ProductController::class,'show'])->name("products.show");
 Route::get('Products/create',[ProductController::class,'create'])->name("products.create");
@@ -53,5 +54,14 @@ Route::post('Products',[ProductController::class,'store'])->name("products.store
 Route::get('Products/{Product}/edit',[ProductController::class,'edit'])->name("products.edit");
 Route::patch('Products/{Product}',[ProductController::class,'update'])->name("products.update");
 Route::delete('Products/{Product}',[ProductController::class,'destroy'])->name("products.destroy");
+/*
+Route::resource('Products',ProductController::class)->only([
+    'index','show','store','update','destroy'
+]);*/
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
+require __DIR__.'/auth.php';
